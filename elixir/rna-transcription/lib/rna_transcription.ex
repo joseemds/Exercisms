4ceba971a45b18afb4effa1cb9cd4b16f870469b
José Eduardo <josee.edumont@gmail.com>
2020-07-30 15:02:35 -0300

    Added convert char

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,33 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+  end
+
+  @spec convert_char(char) :: char | tuple
+  def convert_char(char) do
+    case char do
+      'A' ->
+        'U'
+
+      'C' ->
+        'G'
+
+      'T' ->
+        'A'
+
+      'G' ->
+        'C'
+
+      _ ->
+        {:error, "Invalid Char"}
+    end
+  end
+end

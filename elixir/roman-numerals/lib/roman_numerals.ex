434b3b3871f4febc322e67c73de4ffa43c117eb2
José Eduardo <josee.edumont@gmail.com>
2020-08-04 14:29:29 -0300

    Removed unused function and set convert private function

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -5,17 +5,14 @@ defmodule RomanNumerals do
 
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
+    convert(number)
   end
 
-  def to_roman(number, roman_string) do
-    roman_string <> to_roman(number, roman_string)
-  end
-
-  def convert(n) when n >= 1000, do: "M" <> convert(n - 1000)
-  def convert(n) when n >= 100, do: "C" <> convert(n - 100)
-  def convert(n) when n >= 50, do: "L" <> convert(n - 50)
-  def convert(n) when n >= 10, do: "X" <> convert(n - 10)
-  def convert(n) when n >= 5, do: "V" <> convert(n - 5)
-  def convert(n) when n >= 1, do: "I" <> convert(n - 1)
-  def convert(_), do: ""
+  defp convert(n) when n >= 1000, do: "M" <> convert(n - 1000)
+  defp convert(n) when n >= 100, do: "C" <> convert(n - 100)
+  defp convert(n) when n >= 50, do: "L" <> convert(n - 50)
+  defp convert(n) when n >= 10, do: "X" <> convert(n - 10)
+  defp convert(n) when n >= 5, do: "V" <> convert(n - 5)
+  defp convert(n) when n >= 1, do: "I" <> convert(n - 1)
+  defp convert(_), do: ""
 end

b9e2f6687080a6a7ee54ea024a2ff8caa0bfada0
José Eduardo <josee.edumont@gmail.com>
2020-08-03 21:45:56 -0300

    Added convert function to convert number into roman numeral

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -2,7 +2,15 @@ defmodule RomanNumerals do
   @doc """
   Convert the number to a roman number.
   """
+
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
   end
+
+  def convert(n) when n > 1000, do: "M"
+  def convert(n) when n > 100, do: "C"
+  def convert(n) when n > 50, do: "L"
+  def convert(n) when n > 10, do: "X"
+  def convert(n) when n > 5, do: "V"
+  def convert(n), do: "I"
 end

daba3585b721eb67b6c45e5c4b660f467d857b34
José Eduardo <josee.edumont@gmail.com>
2020-08-03 21:54:29 -0300

    Added logic to convert a number into string, dont know hot remove the necessary value from the number

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -7,10 +7,14 @@ defmodule RomanNumerals do
   def numeral(number) do
   end
 
-  def convert(n) when n > 1000, do: "M"
-  def convert(n) when n > 100, do: "C"
-  def convert(n) when n > 50, do: "L"
-  def convert(n) when n > 10, do: "X"
-  def convert(n) when n > 5, do: "V"
-  def convert(n), do: "I"
+  def to_roman(number, roman_string) do
+    roman_string <> to_roman(number, roman_string)
+  end
+
+  def convert(n) when n >= 1000, do: "M"
+  def convert(n) when n >= 100, do: "C"
+  def convert(n) when n >= 50, do: "L"
+  def convert(n) when n >= 10, do: "X"
+  def convert(n) when n >= 5, do: "V"
+  def convert(n) when n >= 1, do: "I"
 end

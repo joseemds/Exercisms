bceeb81d9add81a4a07d496c401b19dcded6b263
José Eduardo <josee.edumont@gmail.com>
2020-08-05 20:18:14 -0300

    Added some matching to bob, some arent matching

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,7 +1,20 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      String.equivalent?("", String.trim(input)) ->
+        "Fine. Be that way!"
+
+      String.match?(input, ~r/^[^a-z]*$/) && String.match?(input, ~r/[?]$/) ->
+        "Calm down, I know what I'm doing!"
+
+      String.trim(input) |> String.ends_with?("?") ->
+        "Sure."
+
+      String.match?(input, ~r/^[^a-z]*$/) ->
+        "Whoa, chill out!"
+
+      true ->
+        "Whatever."
     end
   end
 end

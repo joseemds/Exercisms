8a95ffbe48455b4f86abc9eb2b21b1ace6ed4894
José Eduardo <josee.edumont@gmail.com>
2020-08-06 13:27:21 -0300

    finished verse function

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,8 +3,32 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
   def verse(number) do
-    # Your implementation here...
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
   end
 
   @doc """

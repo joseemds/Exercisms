90077cbb75335eb8123b8951b995d41bee80274e
José Eduardo <josee.edumont@gmail.com>
2020-08-05 22:59:22 -0300

    Finished exercised, maybe will do some refactoring here

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,11 +1,19 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.ends_with?(input, "?") && String.equivalent?(input, String.upcase(input)) ->
-        "Calm down, I know what I'm doing!"
-
       String.equivalent?("", String.trim(input)) ->
         "Fine. Be that way!"
+
+      String.equivalent?(input, String.upcase(input)) &&
+        String.equivalent?(input, String.downcase(input)) && String.ends_with?(input, "?") ->
+        "Sure."
+
+      String.equivalent?(input, String.upcase(input)) &&
+          String.equivalent?(input, String.downcase(input)) ->
+        "Whatever."
+
+      String.ends_with?(input, "?") && String.equivalent?(input, String.upcase(input)) ->
+        "Calm down, I know what I'm doing!"
 
       String.equivalent?(input, String.upcase(input)) ->
         "Whoa, chill out!"

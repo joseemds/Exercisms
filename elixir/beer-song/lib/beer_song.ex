d8d45a61b98398ec84b20b73b8b3b6bade847db5
José Eduardo <josee.edumont@gmail.com>
2020-08-06 13:43:40 -0300

    Tried to implement lyrics, but is not working as expected

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -35,7 +35,16 @@ defmodule BeerSong do
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
+
   def lyrics(range) do
-    # Your implementation here...
+    Enum.map(range, fn x ->
+      case x do
+        _ -> verse(x)
+      end
+    end)
+  end
+
+  def lyrics(_) do
+    Enum.map(r = 99..0, fn x -> verse(x) end)
   end
 end

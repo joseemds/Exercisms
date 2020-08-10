e53f0be0d73341801803b94fceac111c0e99237e
José Eduardo <josee.edumont@gmail.com>
2020-08-10 20:46:54 -0300

    Finishe two fer clojure

--- clojure/two-fer/src/two_fer.clj
+++ clojure/two-fer/src/two_fer.clj
@@ -1,5 +1,5 @@
 (ns two-fer)
 
-(defn two-fer [name] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn two-fer;; <- arglist goes here
+  ([] (two-fer "you"))
+  ([name] (str "One for " name ", one for me.")))

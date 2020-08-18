a507763f422a1a13dedf8233020c0e55143fdf09
José Eduardo <josee.edumont@gmail.com>
2020-08-18 14:16:16 -0300

    Finished leap exercise with haskell

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,5 +1,8 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = "Implementation go here" 
-  
+isLeapYear year 
+  | mod year 100 == 0 && mod year 400 == 0 = True 
+  | mod year 100 == 0 = False 
+  | mod year 4 == 0 = True
+  | otherwise = False

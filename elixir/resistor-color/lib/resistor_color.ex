ef99c826036333387b239a07e997f78e5d6c69b7
José Eduardo <josee.edumont@gmail.com>
2020-08-20 23:01:17 -0300

    Finished colors function

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -1,8 +1,21 @@
 defmodule ResistorColor do
   @moduledoc false
 
+  @colors ~w"
+      black
+      brown
+      red
+      orange
+      yellow
+      green
+      blue
+      violet
+      grey
+      white"
+
   @spec colors() :: list(String.t())
   def colors do
+    @colors
   end
 
   @spec code(String.t()) :: integer()

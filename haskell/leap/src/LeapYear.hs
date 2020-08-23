82be1c29d9fc44d69c5e5d066d5160a0395ab153
José Eduardo <josee.edumont@gmail.com>
2020-08-23 20:54:48 -0300

    Separated guards on leap exercise

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -3,5 +3,6 @@ module LeapYear (isLeapYear) where
 isLeapYear :: Integer -> Bool
 isLeapYear year 
   | mod year 4 /= 0 = False
-  | mod year 100 == 0 = mod year 400 == 0
+  | mod year 400 == 0 = False
+  | mod year 100 == 0 = True 
   | otherwise = True

6ab11cc4ca16d02ee636a4cc52fcd73bb3e09209
José Eduardo <josee.edumont@gmail.com>
2020-08-28 18:02:19 -0300

    Finished anagram exercise

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    Enum.filter(candidates, fn x -> String.upcase(x) != String.upcase(base) end)
+    |> Enum.filter(&compare(base, &1))
   end
+
+  defp compare(base, word) do
+    word
+    |> String.upcase()
+    |> String.graphemes()
+    |> Enum.sort() ==
+      base
+      |> String.upcase()
+      |> String.graphemes()
+      |> Enum.sort()
+  end
+
+  def compare(_), do: false
 end
